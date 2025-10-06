5c3a8eb3b6968f48d9a3122764e4b29b5fa444d0
Kent <asilo.kentbryanv19@gmail.com>
2025-10-07 02:06:00 +0800

    fix: login bug

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,16 +1,21 @@
-import axios from "axios";
+import axios, {
+  type InternalAxiosRequestConfig,
+  type AxiosRequestHeaders,
+} from "axios";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE || "http://localhost:4000/api",
   withCredentials: true,
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   if (typeof window !== "undefined") {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers = config.headers || {};
-      (config.headers as any).Authorization = `Bearer ${token}`;
+      const headers: AxiosRequestHeaders = (config.headers ||
+        {}) as AxiosRequestHeaders;
+      headers.Authorization = `Bearer ${token}`;
+      config.headers = headers;
     }
   }
   return config;
